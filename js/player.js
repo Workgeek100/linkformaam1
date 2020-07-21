88be46fe9bb78dd6a7a36f46d45e991e2ea881e3
Workgeek100 <maybha_70@hotmail.com>
2020-07-21 17:00:19 +0300

    P45

--- js/player.js
+++ js/player.js
@@ -3,12 +3,13 @@ class Player{
        var options = {
            restitution : 0.2,
            density : 10,
-           friction : 0.5
+           friction : 1
        }
-       //this.visible=255;
-       this.runningImage = loadAnimation("images/running1.png","images/running2.png","images/running3.png","images/running4.png","images/running5.png");
+       this.image2 = loadAnimation("images/running1.png","images/running2.png","images/running3.png","images/running4.png","images/running5.png");
        this.image = loadImage("images/standing.png");
        this.body = Bodies.rectangle(x,y,width,height,options);
+       this.x=x;
+       this.y=y;
        this.width = width;
        this.height = height;
        World.add(world,this.body);
@@ -19,11 +20,8 @@ class Player{
         image(this.image,pos.x,pos.y,this.width,this.height);
         push();
         if(keyDown(RIGHT_ARROW)){
-            //this.visible=this.visible-5;
-            //tint(255,10);
-            //image(this.image,pos.x,pos.y,this.width,this.height);
-            animation(this.runningImage, pos.x,pos.y,this.width,this.height);
-            pos.x=pos.x+2;
+           animation(this.image2, pos.x,pos.y,this.width,this.height);
+            pos.x=pos.x+1;
        }
         pop();
         }
